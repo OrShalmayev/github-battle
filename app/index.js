7fda855d7a12a241b1bd4c2d141fe3edef1bf1a8
unknown <shalmayev1993@gmail.com>
2020-05-07 17:46:23 +0300

    first react component

---
+++ app/index.js
@@ -0,0 +1,17 @@
+var React = require('react');
+
+// components have:
+// state
+// lifecycle events
+// UI
+
+class App extends React.Component {
+    render(){
+        return (
+            <React.Fragment>
+                <h1>Hello World</h1>
+            </React.Fragment>
+        )
+    }//END render
+
+}
